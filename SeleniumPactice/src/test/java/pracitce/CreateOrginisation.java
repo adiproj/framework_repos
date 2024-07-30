ad59e097d7cb4882872ad8313ca65e365fca98b8
adity <adity@ADITYA.mshome.net>
2024-07-30 16:06:22 +0530

    njh

---
+++ SeleniumPactice/src/test/java/pracitce/CreateOrginisation.java
@@ -0,0 +1,16 @@
+package pracitce;
+
+import org.testng.annotations.Test;
+
+public class CreateOrginisation {
+	
+	
+	
+	@Test
+	public void OrginisationTest() {
+		
+		System.out.println("hi");
+		
+	}
+
+}
